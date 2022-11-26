1a46c92b3cd7f43864808590ff110298a67ae97f
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2022-11-26 18:33:45 +0100

    Firebase
    Aggiunto Firebase

--- services/firebse-service.js
+++ services/firebse-service.js
@@ -1,83 +1,106 @@
-"use strict"
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, query, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut, setPersistence  } from "firebase/auth";
+import ParseService from "./parse-service";
 
-export default class FirebaseService{
+export default class FirebaseService {
+
+  FIREBASE_KEYS = {
+    apiKey: "AIzaSyASnMtNGmerW903hs1_ioXlNrmH5GG4GTU",
+    authDomain: "emomap-261122.firebaseapp.com",
+    projectId: "emomap-261122",
+    storageBucket: "emomap-261122.appspot.com",
+    messagingSenderId: "743599726772",
+    appId: "1:743599726772:web:00fcf9d99f548ec6d7f7e1"
+  };
+
+  constructor() {
+    
+    if (FirebaseService._instance) {
+      return FirebaseService._instance;
+    }
+    FirebaseService._instance = this;
+
+    this.app = initializeApp(this.FIREBASE_KEYS);
+    this.db = getFirestore(this.app);
+    // this.auth = getAuth(this.app);
+
+    // this.observeAuthState();
+  }
 
 
+  static instance() {
+    return FirebaseService._instance || new FirebaseService();
+  }
 
-  getEmotions(){
-    return {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "properties": {
-            "emoji": 0,
-            "timestamp": 1669474451000,
-            "message": "lorem ipsum",
-            "userId": "pippo"
+  get isAuth() {
+    return this.auth.currentUser !== null;
+  }
+
+  getCodes() {
+    const q = query(collection(this.db, "codes"));
+    onSnapshot(q, (querySnapshot) => {
+      this.dispatchCodes(querySnapshot.docs.map(doc => doc.data()));
+    });
+  }
+
+  observeAuthState() {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        this.dispatchAuthState(true, user);
+      } else {
+        this.dispatchAuthState(false);
+      }
+    });
+  }
+
+  async isLogged() {
+      return new Promise((resolve, reject) =>
+        onAuthStateChanged( this.auth,
+          user => {
+            console.log(user);
+            if (user) {
+              resolve(true);
+            } else {
+              resolve(false);
+            }
           },
-          "geometry": {
-            "coordinates": [
-              9.048290924733578,
-              44.53692475822638
-            ],
-            "type": "Point"
-          }
-        },
-        {
-          "type": "Feature",
-          "properties": {
-            "emoji": 0,
-            "timestamp": 1669474451000,
-            "message": "lorem ipsum",
-            "userId": "pippo"
-          },
-          "geometry": {
-            "coordinates": [
-              9.449291901295965,
-              44.380093347450355
-            ],
-            "type": "Point"
-          }
-        },
-        {
-          "type": "Feature",
-          "properties": {
-            "emoji": 0,
-            "timestamp": 1669474451000,
-            "message": "lorem ipsum",
-            "userId": "pippo"
-          },
-          "geometry": {
-            "coordinates": [
-              8.46601553410855,
-              44.43895449091272
-            ],
-            "type": "Point"
-          }
-        },
-        {
-          "type": "Feature",
-          "properties": {
-            "emoji": 0,
-            "timestamp": 1669474451000,
-            "message": "lorem ipsum",
-            "userId": "pippo"
-          },
-          "geometry": {
-            "coordinates": [
-              9.80085440129696,
-              44.262193360416035
-            ],
-            "type": "Point"
-          }
-        }
-      ]
+          error => reject(error)
+        )
+      )
+  }
+
+  dispatchAuthState(isLogged, user = null) {
+    const event = new CustomEvent("fb-auth-state", {detail: {isLogged, user}});
+    document.dispatchEvent(event);
+  }
+
+  dispatchCodes(codes) {
+    const event = new CustomEvent("fb-codes", {detail: {codes}});
+    document.dispatchEvent(event);
+  }
+
+  authWithGoogle() {
+    setPersistence(this.auth, "local");{
+      const provider = new GoogleAuthProvider();
+      return signInWithPopup(this.auth, provider);
     }
   }
 
-  setEmotion(emotion){
-    return true;
+  logout() {
+    window.location.hash = "/login";
+    return signOut(this.auth);
   }
 
+  getEmotions(callback){
+    const q = query(collection(this.db, "emotions"));
+    onSnapshot(q, (querySnapshot) => {
+      callback(ParseService.createGeoJSON(querySnapshot.docs.map(doc => doc.data())));
+    });
+  }
+
+  setEmotion(emotion){
+    const emotionsRef = collection(this.db, "emotions");
+    addDoc(emotionsRef, emotion);
+  }
 }
