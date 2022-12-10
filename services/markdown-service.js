3d7d1093df54dececfffa0d254b7f8accc02c25f
Nicoletta Magi <nikym6@gmail.com>
2022-12-10 18:43:09 +0100

    markdown service

--- services/markdown-service.js
+++ services/markdown-service.js
@@ -18,11 +18,11 @@ export default class MarkDownService {
 
   markdownToHtml(markdown) {
     let html = markdown
-    .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
-    .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
-    .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
-    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
-    .replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
-	return html; // using trim method to remove whitespace   
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+    .replace(/\*(.*)\*/gim, '<i>$1</i>');
+	return html;   
   }
 }
