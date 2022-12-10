11de0a4e8fe67e84d6957203d83fb0dcdcd53745
solomaj <maggioalessio2@gmail.com>
2022-12-10 14:28:22 +0100

    init style & tag html menu component

--- components/menu-component/menu-component.js
+++ components/menu-component/menu-component.js
@@ -1,30 +1,33 @@
 import { sel, tag } from "../../libs/emo-lib";
 
-class MenuComponent extends HTMLElement {
+export class MenuComponent extends HTMLElement {
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
 
-  connectedCallback() {
-    this.render()
-  }
+    connectedCallback() {
+        this.render()
+    }
 
-  render() {
-    this.shadowRoot.innerHTML = '';
-    this.shadowRoot.append(...this.createHtml());
-  }
+    render() {
+        this.shadowRoot.innerHTML = '';
+        this.shadowRoot.append(...this.createHtml());
+    }
 
-  createCss() {
-    return
-  }
+    createCss() {
+        return
+    }
 
-  createHtml() {
-    return [
-
-    ]
-  }
+    createHtml() {
+        return [
+            tag('style').h(this.createCss()),
+            tag('div').a('class', 'main').c(
+                tag('img').a('src', './emoji/world-smile.svg')
+            )
+        ]
+    }
 }
 
-customElements.define("credits-component", CreditsComponent);
+customElements.define("menu-component", MenuComponent);
